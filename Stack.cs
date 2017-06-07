e2f5d54a7451251e60cf3926ce0e9c4387687256
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-06-07 08:49:43 -0500

    Stack.cs
    A stack is a linear data structure that allow access to the elements from one end called top (index). The operations of stack data structure are, Push (inserting elements), Pop (deleting an element), Peek (retrieving an element), optionally it can also have sorting and searching operations.

---
+++ Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Stack
+    {
+        int stackSize;
+        int[] stackElements;
+        int index;
+        public Stack()
+        {
+            stackSize = 5;
+            stackElements = new int[stackSize];
+            index = -1;
+        }
+        public Stack(int stackSize)
+        {
+            this.stackSize = stackSize;
+            stackElements = new int[stackSize];
+            index = -1;
+        }
+        public bool Push(int element)
+        {
+            throw new NotImplementedException();
+        }
+        public int Pop()
+        {
+            throw new NotImplementedException();
+        }
+        public int Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
