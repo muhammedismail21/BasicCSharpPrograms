d63971c960a10811b8b3a7d5774ce601cd26d060
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-06-14 10:33:10 -0500

    first commit
    Collections.LinkedList example.

---
+++ LinkedListExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class LinkedListExample
+    {
+        public static void Main()
+        {
+            LinkedList<string> programingLanguages = new LinkedList<string>();
+            programingLanguages.AddLast(new LinkedListNode<string>("C#"));
+            programingLanguages.AddLast(new LinkedListNode<string>("Java"));
+            programingLanguages.AddLast(new LinkedListNode<string>("C++"));
+            programingLanguages.AddLast(new LinkedListNode<string>("C"));
+            programingLanguages.AddLast(new LinkedListNode<string>("COBOL"));
+            programingLanguages.AddLast(new LinkedListNode<string>("PASCAL"));
+            
+            LinkedListNode<string> node = programingLanguages.First;
+            while(node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
