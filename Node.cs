3ca914a8a6d9539850c2b17152279fc56ad63d42
Mohammed Ismail <muhammed.ismail21@gmail.com>
2017-06-19 21:57:01 -0500

    Node.cs fist commit
    This class holds the data and a pointer to next node.

---
+++ Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Node
+    {
+        int value;
+
+        public int Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+        Node nextNode;
+
+        public Node NextNode
+        {
+            get { return nextNode; }
+            set { nextNode = value; }
+        }
+        Node prevNode;
+
+        public Node PrevNode
+        {
+            get { return prevNode; }
+            set { prevNode = value; }
+        }
+        public Node()
+        {
+            nextNode = null;
+            prevNode = null;
+        }
+    }
+}
